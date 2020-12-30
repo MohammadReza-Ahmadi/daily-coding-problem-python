8823d79d1c9856ffbc77ac0fc064f3d42fedd768
mohammad-reza <mohammad-reza@mohammad-reza-ubuntu-20.04>
2020-12-30 07:59:37 +0330

    commit longest palindromic substring test

--- test/test_longest_palindromic_substring.py
+++ test/test_longest_palindromic_substring.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest import TestCase
 
 from src.arrays import longest_palindromic_substring
 
@@ -7,32 +6,56 @@ from src.arrays import longest_palindromic_substring
 class TestLongestPalindromicSubstring(unittest.TestCase):
     def test_none(self):
         in_str = None
-        expected = 0
-        actual = longest_palindromic_substring.get_longest_len(in_str)
+        expected = None
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
         self.assertEquals(expected, actual)
 
     def test_one(self):
         in_str = 'a'
-        expected = 1
-        actual = longest_palindromic_substring.get_longest_len(in_str)
+        expected = 'a'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
         self.assertEquals(expected, actual)
 
     def test_two_not_same(self):
         in_str = 'ab'
-        expected = 1
-        actual = longest_palindromic_substring.get_longest_len(in_str)
+        expected = None
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
         self.assertEquals(expected, actual)
 
     def test_two_same(self):
         in_str = 'aa'
-        expected = 2
-        actual = longest_palindromic_substring.get_longest_len(in_str)
+        expected = 'aa'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
         self.assertEquals(expected, actual)
 
-    def test_3(self):
+    def test_aba(self):
         in_str = 'aba'
-        expected = 2
-        actual = longest_palindromic_substring.get_longest_len(in_str)
+        expected = 'aba'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
+        self.assertEquals(expected, actual)
+
+    def test_racecar(self):
+        in_str = 'racecar'
+        expected = 'racecar'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
+        self.assertEquals(expected, actual)
+
+    def test_bbbbbb(self):
+        in_str = 'bbbbbb'
+        expected = 'bbbbbb'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
+        self.assertEquals(expected, actual)
+
+    def test_babfbcb(self):
+        in_str = 'babfbcb'
+        expected = 'bcb'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
+        self.assertEquals(expected, actual)
+
+    def test_babfbck(self):
+        in_str = 'babfbck'
+        expected = 'bfb'
+        actual = longest_palindromic_substring.get_longest_substring(in_str)
         self.assertEquals(expected, actual)
 
 

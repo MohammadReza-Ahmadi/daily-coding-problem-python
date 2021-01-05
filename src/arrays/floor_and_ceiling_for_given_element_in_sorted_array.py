596f52417e1b0abd34ca4db6520432d71aad93cc
MohammadReza-Ahmadi <m.reza79ahmadi@gmail.com>
2021-01-05 21:35:55 +0330

    impl floor and ceiling of sorted array by the help of binary search

--- src/arrays/floor_and_ceiling_for_given_element_in_sorted_array.py
+++ src/arrays/floor_and_ceiling_for_given_element_in_sorted_array.py
@@ -2,22 +2,60 @@ def get_floor_and_ceiling_of_array_m1_linear_search(array: [], x: int):
     #     floor,ceil
     ret = [-1, -1]
     if x < array[0]:
-        ret[0] = -1
         ret[1] = array[0]
+        return ret
+    elif x > array[len(array) - 1]:
+        ret[0] = array[len(array)-1]
         return ret
 
     for i in range(len(array)):
         if array[i] == x:
-            ret[0] = array[i]
-            ret[1] = array[i]
+            ret[0] = ret[1] = array[i]
             return ret
 
-        if array[i] < x < array[i + 1]:
+        if i < len(array) - 1 and array[i] < x < array[i + 1]:
             ret[0] = array[i]
             ret[1] = array[i + 1]
             return ret
     return ret
 
 
-def get_floor_and_ceiling_of_array_m1_binary_search(array: [], x: int):
-    print()
+def get_floor_and_ceiling_of_array_m2_binary_search(array: [], x: int):
+    ret = [-1, -1]
+    if x < array[0]:
+        ret[1] = array[0]
+        return ret
+    if x > array[len(array) - 1]:
+        ret[0] = array[len(array) - 1]
+        return ret
+    return m2_helper(array, x, 0, len(array) - 1)
+
+
+def m2_helper(array: [], x: int, low: int, high: int):
+    # array = [1, 2, 8, 10, 10, 12, 19]
+    ret = [-1, -1]
+    if low > high:
+        return ret
+
+    if x == array[low] or x == array[high]:
+        ret[0] = ret[1] = x
+        return ret
+    else:
+        mid = ((high - low) + 1) // 2
+        if x == array[mid]:
+            ret[0] = ret[1] = x
+            return ret
+        elif array[mid] < x:
+            if x <= array[mid + 1]:
+                ret[0] = array[mid]
+                ret[1] = array[mid + 1]
+                return ret
+            else:
+                return m2_helper(array, x, mid + 2, high)
+        else:
+            if array[mid - 1] < x:
+                ret[0] = array[mid - 1]
+                ret[1] = array[mid]
+                return ret
+            else:
+                return m2_helper(array, x, low, mid - 1)
--- src/arrays/test_floor_and_ceiling_in_sorted_array.py
+++ src/arrays/test_floor_and_ceiling_in_sorted_array.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.arrays.floor_and_ceiling_for_given_element_in_sorted_array import get_floor_and_ceiling_of_array_m1_linear_search
+from src.arrays.floor_and_ceiling_for_given_element_in_sorted_array import \
+    get_floor_and_ceiling_of_array_m1_linear_search, get_floor_and_ceiling_of_array_m2_binary_search
 
 
 class TestFloorAndCeilingInSortedArray(unittest.TestCase):
@@ -11,12 +12,16 @@ class TestFloorAndCeilingInSortedArray(unittest.TestCase):
         expected = [-1, 1]
         actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
         self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
+        self.assertListEqual(actual, expected)
 
     def test_7_1(self):
         array = [1, 2, 8, 10, 10, 12, 19]
         x = 1
         expected = [1, 1]
         actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
+        self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
         self.assertListEqual(actual, expected)
 
     def test_7_2(self):
@@ -25,12 +30,34 @@ class TestFloorAndCeilingInSortedArray(unittest.TestCase):
         expected = [2, 2]
         actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
         self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
+        self.assertListEqual(actual, expected)
 
     def test_7_2(self):
         array = [1, 2, 8, 10, 10, 12, 19]
         x = 5
         expected = [2, 8]
         actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
+        self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
+        self.assertListEqual(actual, expected)
+
+    def test_7_19(self):
+        array = [1, 2, 8, 10, 10, 12, 19]
+        x = 19
+        expected = [19, 19]
+        actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
+        self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
+        self.assertListEqual(actual, expected)
+
+    def test_7_21(self):
+        array = [1, 2, 8, 10, 10, 12, 19]
+        x = 21
+        expected = [19, -1]
+        actual = get_floor_and_ceiling_of_array_m1_linear_search(array, x)
+        self.assertListEqual(actual, expected)
+        actual = get_floor_and_ceiling_of_array_m2_binary_search(array, x)
         self.assertListEqual(actual, expected)
 
 
